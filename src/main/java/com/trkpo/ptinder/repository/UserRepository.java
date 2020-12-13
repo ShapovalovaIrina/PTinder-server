d13b3b24960e3fb3beecc14547bb1950112adfbc
Shapovalova Irina <irishapovalova13@gmail.com>
2020-12-13 17:34:35 +0300

    Ensure if user exist by google id

--- src/main/java/com/trkpo/ptinder/repository/UserRepository.java
+++ src/main/java/com/trkpo/ptinder/repository/UserRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByGoogleId(String googleId);
     List<User> findByAddress(String address);
+    boolean existsByGoogleId(String googleId);
 }
